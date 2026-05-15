a8c5cb635439719be1780b242bbfc1241e70f95e
Vivian Iyaha <90201480+vivianiyaha@users.noreply.github.com>
2026-05-15 08:23:56 +0100

    Update app.py

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ st.set_page_config(
     page_title="Employee KPI Dashboard",
     page_icon="📊",
     layout="wide"
+    initial_sidebar_state="collapsed"
 )
 
 # =========================================================
